7ed1183b17e6e5518b6d9c01009eceb7f1815a77
ishav_gupta <ishav_gupta@epam.com>
2024-05-08 23:34:36 +0530

    first commit

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Base
+{
+    public Base()
+    {
+        Console.WriteLine("ctor from Base class with Name");
+    }
+
+
+    public virtual void DoWork()
+    {
+        Console.WriteLine("DoWork from Base class");
+    }
+
+}
+
+public class A : Base
+{
+    public A()
+    {
+        Console.WriteLine("ctor from A class with Name");
+    }
+
+    public virtual void DoWork()
+    {
+        Console.WriteLine("DoWork from A class");
+    }
+}
+
+// method hiding concept -> 
+
+
+public class B : A
+{
+    public B()
+    {
+        Console.WriteLine("ctor from B class with Name");
+    }
+
+    public new void DoWork()
+    {
+        Console.WriteLine("DoWork from B class");
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_Flow_Assignment_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Base class instance, Base class object
+            Base ab = new Base();
+            ab.DoWork();
+            // Base class do work
+
+            // Base class instance, Child object
+            Base a = new A();
+            a.DoWork(); 
+            // Base do work
+
+            A aa = new A();
+            aa.DoWork();
+            //child do work
+
+
+            Console.WriteLine("+++++++++");
+            // Now Base class has virtual keyword
+            //Base 
+            ab = new Base();
+            ab.DoWork();
+            // Base class do work
+
+            // Base class instance, Child object
+            a = new A();
+            a.DoWork();
+            // Base do work
+
+            aa = new A();
+            aa.DoWork();
+            //Child do work
+
+
+
+            Console.WriteLine("+++++++++");
+            // Now Base class has virtual keyword and child has override
+            //Base 
+            ab = new Base();
+            ab.DoWork();
+            // Base class do work
+
+            // Base class instance, Child object
+            a = new A();
+            a.DoWork();
+            // child do work
+
+            aa = new A();
+            aa.DoWork();
+            //Child do work
+
+
+
+            Console.WriteLine("+++++++++");
+            // Now Child class has new keyword, Base is normal
+            //Base 
+            ab = new Base();
+            ab.DoWork();
+            // Base class do work
+
+            // Base class instance, Child object
+            a = new A();
+            a.DoWork();
+            // Base do work
+
+            aa = new A();
+            aa.DoWork();
+            //Child do work
+
+
+            Console.WriteLine("+++++++++");
+            // Now Child class has new keyword, Base has virtual
+            //Base 
+            ab = new Base();
+            ab.DoWork();
+            // Base class do work
+
+            // Base class instance, Child object
+            a = new A();
+            a.DoWork();
+            // Base do work
+
+            aa = new A();
+            aa.DoWork();
+            //Child do work
+
+            Console.WriteLine("+++++++++");
+            // Now Child class has virtual keyword, Base has virtual
+            //Base 
+            ab = new Base();
+            ab.DoWork();
+            // Base class do work
+
+            // Base class instance, Child object
+            a = new A();
+            a.DoWork();
+            // Base do work
+
+            aa = new A();
+            aa.DoWork();
+            //Child do work
+
+
+
+
+            Console.WriteLine("=================");
+            Console.Read();   
+
+        }
+    }
+}
